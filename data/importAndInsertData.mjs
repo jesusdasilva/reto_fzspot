859296fe5592cfc21a88b03ba1851f038ad94ffd
JesusManuel <jesusdasilva@gmail.com>
2022-05-21 02:07:52 -0400

    refactoring scripts

--- data/importAndInsertData.mjs
+++ data/importAndInsertData.mjs
@@ -2,47 +2,42 @@
 import axios from 'axios';
 import parser from 'xml2json';
 import fs from 'fs';
+import { promisify } from "util";
+import { exec } from "child_process";
 
-console.log('inicio de Scripts')
-
-console.log('Buscando información xml')
-const response = await axios.get('https://fx-nunchee-assets.s3.amazonaws.com/data/sports.xml');
-
-console.log('Parseando info xml a json')
-let valuesJson = parser.toJson(response.data);
-
-console.log('Reemplazar caracteres especiales')
-valuesJson = valuesJson.replace(/$t/g, 'nombre');
-
-
-console.log('Guardando archivo json')
-fs.writeFileSync('sports5.json', valuesJson);
-
-console.log('fin de Scripts')
-
-// const axios = require('axios');
-// const { get } = require('lodash');
-// const { parseString } = require('xml2js');
-
-
-
-// async function getXml() {
-//   try{
-//     const response = await axios.get('https://fx-nunchee-assets.s3.amazonaws.com/data/sports.xml');
+try {
+    console.log('inicio de Scripts')
     
-//     // console.log('lll',response.data);
+    console.log('Buscando información xml')
+    const response = await axios.get('https://fx-nunchee-assets.s3.amazonaws.com/data/sports.xml');
     
-//   }catch(error){
-//     console.log(error)
-//   }
-  
-//   return response.data;
-// }
+    console.log('Parseando info xml a json')
+    let valuesJson = parser.toJson(response.data)
 
-// console.log(getXml())
+    console.log('Reemplazar caracteres especiales')
+    valuesJson = valuesJson.replaceAll('\$t', 'nombre');
+    
+    console.log('Create Object')
+    const valuesObject = JSON.parse(valuesJson);
+    
+    const nameCollection = Object.keys(valuesObject)
+    console.log(`El nombre de la colección es: ${nameCollection}`)
+    
+    console.log('Guardando archivo json')
+    fs.writeFileSync('sports.json', JSON.stringify(valuesObject[nameCollection]));
+    
+    const execPromise = promisify(exec);
+    
+    async function importJson() {
+      const { stdout, stderr } = await execPromise(`mongoimport "/home/jesus/Desarrollos/fzsport/data/sports.json" -d sports -c ${nameCollection} --drop`);
+      console.log('stdout:', stdout);
+      console.error('stderr:', stderr);
+    }
+    
+    await importJson() 
+    
+    console.log('Fin de Scripts');
+} catch (error) {
+    console.log(error.message);    
+}
 
-// parseString(getXml(), (err, results) => {
-//   const data = JSON.stringify(results);
-
-//   console.log('parseString', data);
-// });
